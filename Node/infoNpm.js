7b922867a50213b092c4a06f9fbb028a226eb0a1
Filipe Couto <Filipe.Couto.10@gmail.com>
2018-10-25 00:30:24 -0300

    informacoes npm. Codigo nao executavel

---
+++ Node/infoNpm.js
@@ -0,0 +1,15 @@
+//Codigo nao executável.
+
+/* O npm é o gerenciador de pacotes do node. 
+ * Com ele é possivel fazer o donwload de pacotes, como a biblioteca Jquery, 
+ * angular, react, etc. Essas bibliotecas são modulos de terceiros e que podem
+ * ser importadas para o seu projeto.
+ * Para instalar use o comando "npm install nome_biblioteca" no terminal.
+ * Ex: para instalar a biblioteca lodash: npm i lodash
+ * Será criada uma nova pasta no projeto chamada node_modules. Dentro de node_modules 
+ * teremos a biblioteca lodash com todos os arquivos .js
+ * crie um arquivo .gitignore na raiz do projeto e adicione a pasta "node_modules"
+ * para não enviar todos esses arquivos ao github.
+ * Com a instalação de bibliotecas o arquivo package-lock.jason será criado.
+ * 
+ */
