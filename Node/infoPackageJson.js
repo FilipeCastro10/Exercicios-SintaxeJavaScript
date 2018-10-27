e71c82d7fb507984cab210bad9c7f9f031ad22c5
Filipe Couto <Filipe.Couto.10@gmail.com>
2018-10-27 19:42:31 -0300

    Node: info sobre package.json e package-lock.json

---
+++ Node/infoPackageJson.js
@@ -0,0 +1,22 @@
+//codigo nao executavel.
+
+/*
+ * o arquivo package.json funciona como descritor do projeto.
+ * Dentro dele há informações relevantes pertencentes ao projeto como 
+ * o repositório no github, a descrição, as dependencias(módulos externos)
+ * instaladas, etc.
+ * Para criar o arquivo package.json use o comando "npm init" no terminal, na pasta
+ * raiz do projeto. Preencha as informações do wizard que será exibido no terminal.
+ * 
+ * Para instalar uma nova dependencia ao projeto: npm i --save nome_da_dependencia
+ * com esse comando a dependencia será instalado e sua descrição adicionada ao 
+ * arquivo package.json para que outros desenvolvedores possam baixar as mesmas
+ * 
+ * o arquivo package-lock.json descreve o projeto e as dependencias de forma mais detalhada.
+ * Incluindo hash de segurança e integridade, garantindo que as dependencias baixadas
+ * serão as mesmas em todos os clones do projeto.
+ * 
+ * 
+ * 
+ * 
+ */
